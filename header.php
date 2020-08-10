8693b362e96118bdfe850adfc0bc58f448ec466a
mathieuradjouki <m.radjouki@codeur.online>
2020-08-10 22:09:50 +0200

    footer

---
+++ header.php
@@ -0,0 +1,210 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Summer exploring It's time for hiking</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <link href="main.css" rel="stylesheet" media="all" type="text/css">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  </head>
+  <body>
+    <div class="container-fluid">
+        <div class="header">
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+              <img src="image/logo.png" alt="logo-site">
+              <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                  <ul class="navbar-nav mr-auto">
+                    <li class="nav-item active">
+                      <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" href="#">Menu</a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" href="#">Our story</a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" href="#">Contact us</a>
+                    </li>
+                  </ul>
+                  <form class="form-inline my-2 my-lg-0">
+                    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                  </form>
+                </div>
+          </nav>
+      </div>
+          <div class="top">
+                  <div class="title">
+                    <h1 class="big-title">IT'S TIME FOR<br>HIKING</h1>
+                    <p class="undertitle">LOREM IPSMUM DOLOR</p>
+                      <div class="p-top">
+                        <button type="button" class="btn-btn-outline-secondary">READ MORE</button>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                      </div>
+                  </div>
+                  <img src="image/img4big.jpg" class="img-fluid" alt="hiking">
+          </div>
+        <div class="container">
+          <div class="let-go">
+          <h2>LET'S GO</h2>
+          <p class="subletgo">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <div class="card-deck">
+              <div class="card">
+                  <img src="image/img1.jpg" class="card-img-top" alt="image1">
+                    <div class="card-body">
+                      <h5 class="card-title">Lorem ipsum</h5>
+                      <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                    </div>
+              </div>
+              <div class="card">
+                  <img src="image/img7.jpg" class="card-img-top" alt="image7">
+                    <div class="card-body">
+                      <h5 class="card-title">Lorem ipsum</h5>
+                      <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                    </div>
+              </div>
+              <div class="card">
+                  <img src="image/img6.jpg" class="card-img-top" alt="image6">
+                    <div class="card-body">
+                      <h5 class="card-title">Lorem ipsum</h5>
+                      <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                    </div>
+              </div>
+            </div>
+            <button class="btn btn-primary btn-lg">SHOW MORE</button>
+        </div>
+        </div>
+          <div class="row row-cols-1 row-cols-md-2">
+            <div class="col-mb-4">
+              <div class="card-step-one">
+                <img src="image/img7.jpg" class="card-img-top" alt="image7">
+              </div>
+            </div>
+            <div class="col-mb-4">
+              <div class="card-step-one">
+                <div class="card-right">
+                  <h4 class="card-title">Dicount up to 50% All Excursions</h4>
+                  <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+                  <button type="button" class="btn-btn-outline-secondary">READ MORE</button>
+                </div>
+              </div>
+            </div>
+            <div class="col-mb-4">
+              <div class="card-step-one">
+                <div class="card-left">
+                  <h4 class="card-title">January's Promo:<br>Buy 1 Get 1 Free!</h4>
+                  <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+                  <button type="button" class="btn-btn-outline-secondary">READ MORE</button>
+                </div>
+              </div>
+            </div>
+            <div class="col-mb-4">
+              <div class="card-step-one">
+                <img src="image/img10.jpg" class="card-img-top" alt="image10">
+              </div>
+            </div>
+          </div>
+          <div class="hiking-mountains">
+              <div class="mountains-text">
+                <h2 class="mountains-title">HIKING THE MOUNTAINS</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+              </div>
+              <img src="image/fond1.jpg" alt="fond">
+          </div>
+          <div class="explore">
+            <img src="image/img11" alt="image11">
+          </div>
+          <div class="part-five">
+            <div class="card-group">
+              <div class="card five" >
+                <div class="body-five" class="card-body">
+                  <h5 class="card-title five-card-title">Lorem Ipsum</h5>
+                  <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exerc.</p>
+                </div>
+              </div>
+              <div class="card five">
+                <div class="body-five" class="card-body">
+                  <h5 class="card-title five-card-title">Lorem Ipsum</h5>
+                  <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercit.</p>
+                </div>
+              </div>
+              <div class="card five">
+                <div class="body-five" class="card-body">
+                  <h5 class="card-title five-card-title">Lorem Ipsum</h5>
+                  <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercit.</p>
+                </div>
+              </div>
+            </div>
+            <div class="text-center text-five">
+              <img src="image/logofive.png" alt="logofive" class="logofive" class="rounded">
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+              <p class="five-subtitle">Lorem Ipsum</p>
+            </div>
+          </div>
+          <div class="subscribe">
+            <div class="six-text">
+              <h2 class="six-title">Title Here</h2>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+              <div class="input-group mb-3">
+                <input type="text" class="form-control" placeholder="your email" aria-label="Recipient's email" aria-describedby="basic-addon2">
+                  <div class="input-group-append">
+                    <span class="input-group-text" id="basic-addon2">SUBSCRIBE</span>
+                  </div>
+              </div>
+            </div>
+            <img src="image/backgroundsix-bell.jpg" alt="background" class="img-fluid">
+          </div>
+          <div class="footer">
+            <div class="card-group" id="footer-card-group">
+              <div class="card card-footer">
+                <div class="card-body card-footer-body">
+                  <h5 class="card-title">Title Here</h5>
+                  <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                </div>
+              </div>
+              <div class="card card-footer">
+                <div class="card-body card-footer-body">
+                  <h5 class="card-title">About</h5>
+                  <p class="card-text">
+                    <ul>
+                      <li>History</li>
+                      <li>Our Team</li>
+                      <li>Brand Guidelines</li>
+                      <li>Terms&Condition</li>
+                      <li>Privacy Policy</li>
+                    </ul>
+                  </p>
+                </div>
+              </div>
+              <div class="card card-footer">
+                <div class="card-body card-footer-body">
+                  <h5 class="card-title">Services</h5>
+                  <p class="card-text">
+                  <ul>
+                    <li>How to Order</li>
+                    <li>Our Product</li>
+                    <li>Our Status</li>
+                    <li>Promo</li>
+                    <li>Payment Method</li>
+                  </ul></p>
+                </div>
+              </div>
+              <div class="card card-footer">
+                <div class="card-body card-footer-body">
+                  <h5 class="card-title">Other</h5>
+                  <p class="card-text">
+                    <ul>
+                      <li>Contact us</li>
+                      <li>Help</li>
+                      <li>Privacy</li>
+                    </ul></p>
+                </div>
+              </div>
+            </div>
+            
+          </div>
+      </div>
